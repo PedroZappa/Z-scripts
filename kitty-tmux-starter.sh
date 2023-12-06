376718d2f32d2060c1c374c7d763aa36a1fd06f7
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-06 18:30:47 +0000

    kitty-tmux-starter : Added Z-scripts tmux tab to RC

--- kitty-tmux-starter.sh
+++ kitty-tmux-starter.sh
@@ -14,6 +14,10 @@ tmux new-window		-t RC:2 -n 'obsidian' -c $OBSIDIAN_VAULT_PATH
 tmux send-keys		-t RC:2 'll' C-m
 tmux split-window	-t RC:2 -h 
 tmux send-keys		-t RC:2 'obsidian' C-m
+# Create Z-scripts RC window
+tmux new-window		-t RC:3 -n 'Z-scripts'
+tmux send-keys		-t RC:1 'cd ~/C0D3/Z-scripts' C-m
+tmux send-keys		-t RC:3 'll' C-m
 
 #
 # Launch a new kitty tab and attach it to the tmux session "RC"
@@ -22,8 +26,8 @@ kitty @ launch --type=tab sh -c 'tmux attach-session -t RC'
 # Launch another kitty tab and attach it to the tmux session "Z"
 kitty @ launch --type=tab sh -c 'tmux attach-session -t Z'
 
-# Focus the new tab
-# kitty @ focus-tab --match title:Z
 # Create Z session
 tmux new-session	-d -s Z
 tmux attach-session -t Z
+
+kitty @ focus-tab previous_tab
