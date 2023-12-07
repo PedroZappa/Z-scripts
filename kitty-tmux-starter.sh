94d1e6bac2f8286c4694f96c91168d7e5a6ca3e5
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-07 12:25:52 +0000

    kitty-tmux-starter : Fixed Obsidian Paths and added conditional for top in case no btop or htop is found

--- kitty-tmux-starter.sh
+++ kitty-tmux-starter.sh
@@ -2,9 +2,9 @@
 
 # Set Path to Obsidian Vault
 if [[ $USER == "zedr0" ]]; then
-	OBSIDIAN_VAULT_PATH="/home/passunca/sgoinfre/Obsidian/ZedroVault"
+	OBSIDIAN_VAULT_PATH="~/Documents/Obsidian/ZedroVault"
 else [[ $USER == "passunca" ]];
-	OBSIDIAN_VAULT_PATH="/home/zedr0/Documents/Obsidian/ZedroVault"
+	OBSIDIAN_VAULT_PATH="~/sgoinfre/Obsidian/ZedroVault"
 fi
 
 # Create RC session
@@ -19,16 +19,20 @@ tmux send-keys		-t RC:2 'cd ~/C0D3/z-scripts' C-m
 tmux send-keys		-t RC:2 'nvim .' C-m
 # Create obsidian RC window
 tmux new-window		-t RC:3 -n 'obsidian' -c $OBSIDIAN_VAULT_PATH
+tmux send-keys		-t RC:3 'cd '$OBSIDIAN_VAULT_PATH C-m
 tmux send-keys		-t RC:3 'll' C-m
 tmux split-window	-t RC:3 -h 
+tmux send-keys		-t RC:3 'git pull --verbose' C-m
 tmux send-keys		-t RC:3 'obsidian' C-m
 # Create Monitoring RC window
 tmux new-window		-t RC:4 -n 'ztop'
 if command -v btop &> /dev/null
 then
 	tmux send-keys	-t RC:4 'btop' C-m
-else
+else if command -v htop &> /dev/null
 	tmux send-keys	-t RC:4 'htop' C-m
+else
+	tmux send-keys	-t RC:4 'top' C-m
 fi
 
 # Launch a new kitty tab and attach it to the tmux session "RC"
