00fa613839893c263819bcff3b3ef5e52871dc0a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-20 17:06:35 +0000

    zmux-start.sh : 󰙪

--- zmux-start.sh
+++ zmux-start.sh
@@ -18,13 +18,13 @@ tmux rename-window	-t RC:1 '.dotfiles'
 tmux send-keys		-t RC:1 'cd $HOME/.dotfiles' C-m
 tmux send-keys		-t RC:1 'git pull --verbose' C-m
 tmux send-keys		-t RC:2 'glgs' C-m
-tmux send-keys		-t RC:1 'nvim' C-m
+tmux send-keys		-t RC:1 'nvim'
 # Create Z-scripts RC window
 tmux new-window		-t RC:2 -n 'z-scripts'
 tmux send-keys		-t RC:2 'cd $HOME/C0D3/z-scripts' C-m
 tmux send-keys		-t RC:2 'git pull --verbose' C-m
 tmux send-keys		-t RC:2 'glgs' C-m
-tmux send-keys		-t RC:2 'nvim' C-m
+tmux send-keys		-t RC:2 'nvim'
 # Create obsidian RC window
 tmux new-window		-t RC:3 -n 'obsidian' -c $OBSIDIAN_VAULT_PATH
 tmux send-keys		-t RC:3 'cd '$OBSIDIAN_VAULT_PATH C-m
@@ -37,7 +37,7 @@ tmux split-window	-t RC:3 -h
 tmux send-keys		-t RC:3 'cd '$OBSIDIAN_VAULT_PATH C-m
 tmux send-keys		-t RC:3 'git pull --verbose' C-m
 tmux send-keys		-t RC:2 'glgs' C-m
-tmux send-keys		-t RC:3 'obsidian' C-m
+tmux send-keys		-t RC:3 'obsidian'
 # Create Monitoring RC window
 tmux new-window		-t RC:4 -n 'ztop'
 if command -v btop &> /dev/null; then
