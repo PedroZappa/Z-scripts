10bc27c5723e3b9514c6a63aceeee43e5e9ccc86
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-28 20:33:54 +0000

    zmux-start.sh : 󰙪

--- zmux-start.sh
+++ zmux-start.sh
@@ -16,28 +16,28 @@ tmux new-session	-d -s RC
 # Create .dotfiles RC window
 tmux rename-window	-t RC:1 '.dotfiles'
 tmux send-keys		-t RC:1 'cd $HOME/.dotfiles' C-m
-tmux send-keys		-t RC:1 'git pull --verbose' C-m
+tmux send-keys		-t RC:1 'git pull' C-m
 tmux send-keys		-t RC:2 'glgs' C-m
-tmux send-keys		-t RC:1 'nvim'
+tmux send-keys		-t RC:1 'nvim' C-m
 # Create Z-scripts RC window
 tmux new-window		-t RC:2 -n 'z-scripts'
 tmux send-keys		-t RC:2 'cd $HOME/C0D3/z-scripts' C-m
-tmux send-keys		-t RC:2 'git pull --verbose' C-m
+tmux send-keys		-t RC:2 'git pull' C-m
 tmux send-keys		-t RC:2 'glgs' C-m
-tmux send-keys		-t RC:2 'nvim'
+tmux send-keys		-t RC:2 'nvim' C-m
 # Create obsidian RC window
 tmux new-window		-t RC:3 -n 'obsidian' -c $OBSIDIAN_VAULT_PATH
 tmux send-keys		-t RC:3 'cd '$OBSIDIAN_VAULT_PATH C-m
-if command -v lsd &> /dev/null; then
+if command -v eza &> /dev/null; then
 	tmux send-keys	-t RC:3 'eza -al' C-m
 else
 	tmux send-keys		-t RC:3 'll' C-m
 fi
 tmux split-window	-t RC:3 -h 
 tmux send-keys		-t RC:3 'cd '$OBSIDIAN_VAULT_PATH C-m
-tmux send-keys		-t RC:3 'git pull --verbose' C-m
+tmux send-keys		-t RC:3 'git pull' C-m
 tmux send-keys		-t RC:2 'glgs' C-m
-tmux send-keys		-t RC:3 'obsidian'
+tmux send-keys		-t RC:3 'obsidian' C-m
 # Create Monitoring RC window
 tmux new-window		-t RC:4 -n 'ztop'
 if command -v btop &> /dev/null; then
@@ -46,9 +46,10 @@ else
 	tmux send-keys	-t RC:4 'htop' C-m 
 fi
 
-# Launch a new kitty tab and attach it to the tmux session "RC"
-kitty @ launch --type=tab sh -c 'tmux attach-session -t RC'
 
 # Create Z session
 tmux new-session	-d -s Z
 tmux attach-session -t Z
+
+# Launch a new kitty tab and attach it to the tmux session "RC"
+kitty new_tab -- tmux attach -t RC
