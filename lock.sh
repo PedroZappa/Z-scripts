4314f356188240e880cd9f81fd0cc0ae3dd3444a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-19 20:52:13 +0000

    lock.sh : Added source

--- lock.sh
+++ lock.sh
@@ -5,3 +5,5 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -u -i /tmp/screen.png
 rm /tmp/screen.png
+
+# Source : https://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
