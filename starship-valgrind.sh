a6ee7253cf8883fe9e7b43b3abe275645d6db02c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-24 11:36:03 +0000

    scripts : moved scripts into .dotfiles

--- starship-valgrind.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VALGRIND_PIDS=$(ps aux | grep vgdb | grep -v grep | awk '{print $2}')
-COUNT=$(echo "$VALGRIND_PIDS" | wc -l)
-
-if [ $COUNT -eq 1 ]; then
-	echo "$VALGRIND_PIDS"
-elif [ $COUNT -gt 1 ]; then
-	echo "⚔$COUNT"
-fi
